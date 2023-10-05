510f535dc3b98edfb724e04869fc9c443af74c53
matias-pacheco <matias-pacheco@outlook.es>
2023-10-05 18:22:17 -0300

    Creación Entidad
    Creación:
    - Entidad Comida

---
+++ nutricionista_G52/src/nutricionista_g52/entidades/Comida.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nutricionista_g52.entidades;
+
+/**
+ *
+ * @author Matías Pacheco
+ */
+public class Comida {
+    private int idComida;
+    private String nombre;
+    private String detalle;
+    private int cantCalorias;
+    private boolean estado;
+
+    public Comida(int idComida, String nombre, String detalle, int cantCalorias, boolean estado) {
+        this.idComida = idComida;
+        this.nombre = nombre;
+        this.detalle = detalle;
+        this.cantCalorias = cantCalorias;
+        this.estado = estado;
+    }
+
+    public Comida(String nombre, String detalle, int cantCalorias, boolean estado) {
+        this.nombre = nombre;
+        this.detalle = detalle;
+        this.cantCalorias = cantCalorias;
+        this.estado = estado;
+    }
+    
+    public Comida(){}
+
+    public int getIdComida() {
+        return idComida;
+    }
+    public void setIdComida(int idComida) {
+        this.idComida = idComida;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDetalle() {
+        return detalle;
+    }
+    public void setDetalle(String detalle) {
+        this.detalle = detalle;
+    }
+
+    public int getCantCalorias() {
+        return cantCalorias;
+    }
+    public void setCantCalorias(int cantCalorias) {
+        this.cantCalorias = cantCalorias;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "idComida: "+idComida+"\nnombre: "+nombre+"\ndetalle: "+detalle+"\ncantCalorias: "+cantCalorias
+                +"\nestado: "+estado+"\n";
+    }
+}
